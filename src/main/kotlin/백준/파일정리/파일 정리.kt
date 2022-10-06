2e645e8fc300f46b991e6254c808b1ed56cd2546
hwj <jminie@naver.com>
2022-10-06 18:07:37 +0900

    파일정리 두번째 풀이

--- src/main/kotlin/백준/파일정리/파일 정리.kt
+++ src/main/kotlin/백준/파일정리/파일 정리.kt
@@ -1,17 +1,35 @@
 package 백준.파일정리
 
+// 풀이 1
+//fun main() = with(System.`in`.bufferedReader()) {
+//    val n = readLine()!!.toInt()
+//    val result = mutableListOf<String>()
+//    repeat(n) {
+//        val extension = readLine()!!.split(".")[1]
+//        result.add(extension)
+//    }
+//    val resultGroup = result.groupBy { it }.toList().sortedBy { (key, _) -> key }.toMap()
+//    resultGroup.forEach {
+//        print(it.key)
+//        print(" ")
+//        print(it.value.size)
+//        println()
+//    }
+//}
+
+// 풀이 2
 fun main() = with(System.`in`.bufferedReader()) {
+    val map = mutableMapOf<String, Int>()
     val n = readLine()!!.toInt()
-    val result = mutableListOf<String>()
     repeat(n) {
         val extension = readLine()!!.split(".")[1]
-        result.add(extension)
+        if (map.containsKey(extension)) {
+            map[extension] = map[extension]!! + 1
+        } else {
+            map[extension] = 1
+        }
     }
-    val resultGroup = result.groupBy { it }.toList().sortedBy { (key, _) -> key }.toMap()
-    resultGroup.forEach {
-        print(it.key)
-        print(" ")
-        print(it.value.size)
-        println()
+    map.toSortedMap().forEach {
+        println("${it.key} ${it.value}")
     }
 }
