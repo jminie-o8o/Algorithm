de88b66a740dedb46143d249e261a11fca02b7b6
hwj <jminie@naver.com>
2022-10-19 16:35:28 +0900

    하노이 탑 해결

---
+++ src/main/kotlin/백준/하노이 탑/하노이 탑.kt
@@ -0,0 +1,21 @@
+package 백준.`하노이 탑`
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readLine()!!.toInt()
+    val sb = StringBuilder()
+    var count = 0
+    fun hanoi(roundCount: Int, start: Int, target: Int, assist: Int) {
+        if (roundCount == 1) {
+            sb.append("$start $target\n")
+            count += 1
+            return
+        }
+        hanoi(roundCount - 1, start, assist, target)
+        sb.append("$start $target\n")
+        count += 1
+        hanoi(roundCount - 1, assist, target, start)
+    }
+    hanoi(n, 1, 3, 2)
+    println(count)
+    println(sb)
+}
