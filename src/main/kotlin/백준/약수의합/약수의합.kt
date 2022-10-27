87b8afc1c66e9946298c84255ad9a8889cf30856
hwj <jminie@naver.com>
2022-10-27 16:00:18 +0900

    약수의 합 주석 추가

--- src/main/kotlin/백준/약수의합/약수의합.kt
+++ src/main/kotlin/백준/약수의합/약수의합.kt
@@ -7,16 +7,18 @@ val g = LongArray(MAX){1}
 
 // 각 숫자의 약수의 합을 담아준다
 // 즉 f[n]을 완성
+// 시간복잡도 O(lonN)
 fun solveF(){
     for(i in 2 until MAX){
         for(j in i until MAX step i){
-            g[j]+=i.toLong()
+            g[j] += i.toLong()
         }
     }
 }
 
 // 지금까지 숫자의 약수의 합을 구해준다
 // 즉 g[n]을 완성
+// 시간복잡도 O(N)
 fun solveG(){
     for(i in 2 until MAX){
         g[i] += g[i-1]
