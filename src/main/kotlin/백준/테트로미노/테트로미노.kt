9ed6fcc2ed456660f1eb5008f7b1e69bd96111b3
hwj <jminie@naver.com>
2022-11-09 15:37:32 +0900

    백준 테트로미노 해결

---
+++ src/main/kotlin/백준/테트로미노/테트로미노.kt
@@ -0,0 +1,56 @@
+package 백준.테트로미노
+
+import java.util.*
+import kotlin.math.*
+
+var answer = 0
+
+fun dfs(x: Int, y: Int, step: Int, total: Int, visited: Array<BooleanArray>, n: Int, m: Int, board: Array<IntArray>) {
+    // 종료조건 : 블록 4개를 모두 활용한 경우
+    if (step == 4) {
+        answer = max(answer, total)
+        return
+    }
+    val d = listOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1)) // 좌표 내 상하좌우
+    for ((dx, dy) in d) {
+        val nx = x + dx // 다음 x 좌표
+        val ny = y + dy // 다음 y 좌표
+        // 새로운 좌표가 유요한 범위 내에 있고 탐색 이력이 없는 경우
+        if ((nx in 0 until n) && (ny in 0 until m) && !visited[nx][ny]) {
+            // 2번째 블록까지 연결 한 후 ㅏ ㅗ ㅜ ㅓ 만들기
+            if (step == 2) {
+                visited[nx][ny] = true // 탐색 기록 추가
+                // 새로운 좌표에서 탐색하지 않고 기존 좌표(x, y)에서 탐색
+                dfs(x, y, step + 1, total + board[nx][ny], visited, n, m ,board)
+                visited[nx][ny] = false // 탐색 기록 삭제
+            }
+            visited[nx][ny] = true
+            // 새로운 좌표에서 (nx, ny)에서 탐색
+            dfs(nx, ny, step + 1, total + board[nx][ny], visited, n, m ,board)
+            visited[nx][ny] = false
+        }
+    }
+}
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine()!!.split(" ").map { it.toInt() }
+    val board = Array(n) { IntArray(m) }
+    for (i in 0 until n) {
+        val st = StringTokenizer(readLine())
+        for (j in 0 until m) {
+            board[i][j] = st.nextToken().toInt()
+        }
+    }
+    val visited = Array(n) { BooleanArray(m) { false } }
+
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            visited[i][j] = true
+            dfs(i, j, 1, board[i][j], visited, n, m, board)
+            visited[i][j] = false
+        }
+    }
+    println(answer)
+}
+
+
--- src/main/kotlin/테스트/테스트.kt
+++ src/main/kotlin/테스트/테스트.kt
@@ -1,3 +1,78 @@
 package 테스트
 
+val numberSet = HashSet<String>()
+
+fun recursive(comb: String, others: String) {
+    // 1. 현재 조합을 set 에 추가한다. (Set 이므로 중복을 알아서 처리)
+    if (comb != "") numberSet.add(comb)
+    // 2. 남은 숫자 중 한개를 더 해 새로운 조합을 만든다.
+    for (i in others.indices) {
+        recursive(comb + others[i], others.substring(0, i) + others.substring(i + 1))
+    }
+}
+
+fun <T> combination(
+    answer: MutableList<List<T>>,
+    el: List<T>,
+    ck: Array<Boolean>,
+    start: Int,
+    target: Int
+) {
+    if (target == 0) {
+        answer.addAll(listOf(el.filterIndexed { index, _ -> ck[index] }))
+    } else {
+        for (i in start until el.size) {
+            ck[i] = true
+            combination(answer, el, ck, i + 1, target - 1)
+            ck[i] = false
+        }
+    }
+}
+
+fun main() {
+    val test2 = listOf("a", "b", "c")
+    recursive("", test2.joinToString(""))
+    println(numberSet)
+    println(numberSet.filter { it.length == 3 })
+
+    val test3 = listOf("a", "b", "c")
+    val answer = mutableListOf<List<String>>()
+    val ck = Array(test3.size) { false }
+    combination(answer, test3, ck, 0, 2)
+    println(answer)
+}
+
+
+// 순열 (순서를 고려하므로 순서가 바뀌면 다른 것으로 취급)
+val numberSetTest = HashSet<String>()
+
+fun permutation(str: String, others: String) {
+    if (str != "") numberSet.add(str)
+    for (i in others.indices) {
+        permutation(str + others[i], others.substring(0, i) + others.substring(i + 1))
+    }
+}
+
+// 조합 (순서를 고려하지 않으므로 순서가 바뀌어도 같은 것으로 취급)
+fun <T> combinationTest(
+    answer: MutableList<List<T>>,
+    el: List<T>,
+    ck: Array<Boolean>,
+    start: Int,
+    target: Int
+) {
+    if (target == 0) answer.addAll(listOf(el.filterIndexed { index, _ -> ck[index] }))
+    else {
+        for (i in start until el.size) {
+            ck[i] = true
+            combinationTest(answer, el, ck, i + 1, target - 1)
+            ck[i] = false
+        }
+    }
+}
+
+
+
+
+
 
